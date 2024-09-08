a2ebc115f8512ef2f06a577c80f84e6ea0b74244
Willian <awillian0@gmail.com>
2024-09-08 11:07:04 -0300

    fix(navbar): Fix navbar responsive items

--- src/components/responsive-menu.tsx
+++ src/components/responsive-menu.tsx
@@ -11,10 +11,17 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { ChevronDown, UserCircle } from 'lucide-react';
+import Image from "next/image";
 
 const ResponsiveMenu: React.FC = () => {
   const { data: session } = useSession();
   const router = useRouter();
+
+  function isMobile() {
+    return window.matchMedia("(max-width: 768px)").matches;
+  }
+
+
 
   if (session) {
     return (
@@ -23,11 +30,18 @@ const ResponsiveMenu: React.FC = () => {
 
           <div className="flex items-center justify-between w-full">
 
-            <div className="space-x-6">
+            <div className="flex flex-row items-center justify-center space-x-6">
 
               {/* Título ou logotipo */}
-              <Link href="/" className="text-xl font-bold mr-4" style={{ fontSize: 22 }}>
-                Controle CC
+              <Link href="/" className="text-xl font-bold mr-4 hidden md:block" style={{ fontSize: 22 }}>
+                <Image
+                  src="/icon.png" // Caminho para sua imagem
+                  alt="Background do curso de Ciência da Computação"
+                  width={30}
+                  height={30}
+                  quality={100}
+                  className="opacity-70"
+                />
               </Link>
 
               {/* Links condicionais baseados no papel do usuário */}
@@ -59,7 +73,11 @@ const ResponsiveMenu: React.FC = () => {
                   aria-label="Abrir menu de usuário"
                 >
                   <UserCircle size={16} className='mr-2 mt-1' />
-                  {session?.user?.name?.split(" ")[0] || "Usuário"}
+                  <div className="hidden md:block">
+                    {
+                      session?.user?.name?.split(" ")[0] || "Usuário"
+                    }
+                  </div>
                   <ChevronDown size={12} className='ml-2 mt-1' />
                 </DropdownMenuTrigger>
 
@@ -67,15 +85,21 @@ const ResponsiveMenu: React.FC = () => {
                   <DropdownMenuLabel>Minha Conta</DropdownMenuLabel>
                   <DropdownMenuSeparator />
 
-                  <DropdownMenuItem onClick={() => router.push("/myprogress")}>
-                    Meu Progresso
-                  </DropdownMenuItem>
+                  {
+                    session?.user.role !== "admin" && (
+                      <>
+                        <DropdownMenuItem onClick={() => router.push("/myprogress")}>
+                          Meu Progresso
+                        </DropdownMenuItem>
 
-                  <DropdownMenuItem onClick={() => { }}>
-                    Notificações
-                  </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => { }}>
+                          Notificações
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                      </>
+                    )
+                  }
 
-                  <DropdownMenuSeparator />
 
                   <DropdownMenuItem onClick={() => signOut({ callbackUrl: "/login" })}>
                     Logout
