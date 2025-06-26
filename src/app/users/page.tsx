b9d3988dc4b35e55c04ed8729998ba3a5f7a2fa3
Willian A. dos Santos <willian@playsaas.com>
2025-06-26 11:19:25 -0300

    Update users page

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,7 +2,6 @@ import dynamic from 'next/dynamic';
 
 const UsersPage = dynamic(() => import('@/components/users-page'), { ssr: false });
 
-
 export default function Page() {
   return <UsersPage />;
 } 
